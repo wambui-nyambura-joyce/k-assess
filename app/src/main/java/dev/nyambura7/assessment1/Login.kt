8bc5c9d70e4df3e1b8f8c5378245299df10c6384
wambui-nyambura <nyamburawambuui@gmail.com>
2023-06-20 10:36:38 +0300

    Made changes

--- app/src/main/java/dev/nyambura7/assessment1/Login.kt
+++ app/src/main/java/dev/nyambura7/assessment1/Login.kt
@@ -3,24 +3,55 @@ package dev.nyambura7.assessment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dev.nyambura7.assessment1.databinding.ActivityLoginBinding
-import dev.nyambura7.assessment1.databinding.LoginpageBinding
 
 class Login : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityLoginBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-//            binding.btnLogin.setOnClickListener {
-//                // TODO: Perform login logic here
-//            }
-//
-//            binding.tvSignup.setOnClickListener {
-//                startActivity(Intent(this, LoginpageBinding::class.java))
-//                finish()
-//            }
-        }
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-//}
+
+    override fun onResume() {
+        super.onResume()
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        validateSignUp()
+    }
+
+    fun validateSignUp() {
+        val name = binding.etUsername.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etpassword.text.toString()
+        var error = false
+
+        if (name.isBlank()) {
+            binding.tilUserName.error = "Name field cannot be empty"
+            error = true
+        } else {
+            binding.tilUserName.error = null
+        }
+        if (email.isBlank()) {
+            binding.tilEmail.error = "Email field cannot be empty"
+            error = true
+        } else {
+            binding.tilEmail.error = null
+        }
+        if (password.isBlank()) {
+            binding.tilpassword.error = "Password field cannot be empty"
+            error = true
+        } else {
+            binding.tilpassword.error = null
+        }
+        if (!error) {
+            Toast.makeText(this, "Contact Successfully added", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+}
+
+
--- app/src/main/java/dev/nyambura7/assessment1/MainActivity.kt
+++ app/src/main/java/dev/nyambura7/assessment1/MainActivity.kt
@@ -3,8 +3,8 @@ package dev.nyambura7.assessment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dev.nyambura7.assessment1.databinding.ActivityMainBinding
-import dev.nyambura7.assessment1.databinding.LoginpageBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,8 +13,64 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
         binding.btnSignup.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+
+            startActivity(Intent(this, Login::class.java))
+        }
+        binding.btnLogin2.setOnClickListener {
+            startActivity(Intent(this,Login::class.java))
+        }
+        validateLogin()
+
+    }
+
+
+    fun validateLogin(){
+        val name = binding.etUserName.text.toString()
+        val email = binding.etEmail.text.toString()
+        val phone = binding.etPhoneNumber.text.toString()
+        val password = binding.etPassword.text.toString()
+        val passwordconf = binding.etPasswordconf.text.toString()
+        var error = false
+
+        if (name.isBlank()){
+            binding.tilUserName.error = "Name field cannot be empty"
+            error = true
+        }else{
+            binding.tilUserName.error=null
+        }
+        if (email.isBlank()){
+            binding.tilEmail.error = "Email field cannot be empty"
+            error = true
+        }else{
+            binding.tilEmail.error=null
+        }
+        if (password.isBlank()){
+            binding.tilPassword.error = "Password field cannot be empty"
+            error = true
+        }else{
+            binding.tilPassword.error=null
+        }
+        if (password!=passwordconf){
+            binding.etPasswordconf.error = "Passwords do not match"
+            error = true
+        }else{
+            binding.tilPasswordconf.error = null
+        }
+
+        if (phone.isBlank()){
+            binding.tilPhoneNumber.error = "Phone number field cannot be empty"
+            error = true
+        } else{
+            binding.tilPhoneNumber.error = null
+        }
+
+        if (!error){
+            Toast.makeText(this, "Contact Successfully added", Toast.LENGTH_LONG).show()
         }
     }
 
